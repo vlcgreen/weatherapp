d4154b3a1d6a586fc69a41a447f36ca0ef317773
Victoria Green <victoria.cousan@gmail.com>
2021-05-11 13:54:40 -0500

    search funct

--- js/weatherapp.js
+++ js/weatherapp.js
@@ -1,32 +1,63 @@
 
 
-// var locInput = document.getElementById("cityinput").value;
-//     console.log(locInput)
-var locInput = "Houston"
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locInput}&units=imperial&appid=${apiKey}`)
-    .then(response=>response.json())
-    .then(data=>{
-        console.log(data)
-
-    const temp1 = data.main.temp
-    const winds1 = data.wind.speed
-    const feels1 = data.main.feels_like
-
-    let temp = document.getElementById("temp1")
-    let feels = document.getElementById("feels1")
-    let winds = document.getElementById("clouds1")
-
-    temp.textContent = `Temp: ${temp1.toString()}\u00B0`
-    feels.textContent = `Feels Like: ${feels1.toString()}\u00B0`
-    winds.textContent = `Wind Speed: ${winds1} mph`
-
+    var locInput = document.getElementById("cityinput")
+    locInput.addEventListener('keyup', (search)=>{
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.target.value}&units=imperial&appid=${apiKey}`)
+        .then(response=>response.json())
+        .then(data=>{
+            console.log(data)
     
-
-    if (temp1 > 80){
-        var image = document.getElementById("image")
-        image.src="images/hot.png"
-    }
+        const temp1 = data.main.temp
+        const winds1 = data.wind.speed
+        const feels1 = data.main.feels_like
     
-
+        let temp = document.getElementById("temp1")
+        let feels = document.getElementById("feels1")
+        let winds = document.getElementById("clouds1")
+    
+        temp.textContent = `Temp: ${temp1.toString()}\u00B0`
+        feels.textContent = `Feels Like: ${feels1.toString()}\u00B0`
+        winds.textContent = `Wind Speed: ${winds1} mph`
+    
+        
+    
+        if (temp1 > 80){
+            var image = document.getElementById("image")
+            image.src="images/hot.png"
+        }
+        else if (temp1 < 70){ 
+            var image =document.getElementById('image') 
+            image.src= ""
+        }
+        
+    
+        })
     })
+
+    // fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locInput}&units=imperial&appid=${apiKey}`)
+    // .then(response=>response.json())
+    // .then(data=>{
+    //     console.log(data)
+
+    // const temp1 = data.main.temp
+    // const winds1 = data.wind.speed
+    // const feels1 = data.main.feels_like
+
+    // let temp = document.getElementById("temp1")
+    // let feels = document.getElementById("feels1")
+    // let winds = document.getElementById("clouds1")
+
+    // temp.textContent = `Temp: ${temp1.toString()}\u00B0`
+    // feels.textContent = `Feels Like: ${feels1.toString()}\u00B0`
+    // winds.textContent = `Wind Speed: ${winds1} mph`
+
+    
+
+    // if (temp1 > 80){
+    //     var image = document.getElementById("image")
+    //     image.src="images/hot.png"
+    // }
+    
+
+    // })
